2cafa43b44f4e9ce27f230663be3a404395c3f2c
25A31A0506 <sandhyadasari1527@gmail.com>
2026-09-16 21:17:26 +0530

    Create arthimatic_operation.py

---
+++ numbers/arthimatic_operation.py
@@ -0,0 +1,11 @@
+num1=int(input())
+num2=int(input())
+if num1>0 and num2>0:
+    sum_=num1+num2
+    Difference=num1-num2
+    product=num1*num2
+    Division=num1/num2
+print("sum=",sum_)
+print("Difference=",Difference)
+print("product=",product)
+print("Division=",Division)
