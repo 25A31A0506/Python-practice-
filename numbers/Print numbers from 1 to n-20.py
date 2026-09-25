219b3469ebeb046ed43e87199f917d0e0f868606
25A31A0506 <sandhyadasari1527@gmail.com>
2026-09-25 23:04:27 +0530

    Create Prime numbers from 1 to n-20.py

---
+++ numbers/Print numbers from 1 to n-20.py
@@ -0,0 +1,9 @@
+#Print all Prime Numbers from 1 to N
+num=int(input("Enter a number:"))
+for i in range(1,num+1):
+    count=0
+    for j in range(1,i+1):
+        if i%j==0:
+            count+=1
+    if count==2:
+        print(i)
